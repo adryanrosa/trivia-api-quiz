b41abdb6d3b355f2f71cfacf0b0250344f9e3173
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-20 18:49:21 -0300

    Modifica Input

--- src/components/styled/Input.js
+++ src/components/styled/Input.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const Input = styled.input`
   width: 100%;
   background-color: transparent;
-  padding: 1em 0.5rem;
+  padding: 0.5rem;
   border: none;
   border-bottom: 0.0625rem solid hsla(0, 0%, 100%, 62.5%);
   border-radius: 0.125rem;
+  font-size: ${({ theme }) => theme.fontSizes['400']};
   color: ${({ theme }) => theme.colors.neutral100};
 
   &:focus {
