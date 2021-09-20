02eee12832ddc4284f2c435176f0d887601ccc8e
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-20 13:23:59 -0300

    Estiliza Timer

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,7 +1,32 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import styled, { ThemeProvider } from 'styled-components';
 
 import { decreaseTime } from '../redux/actions';
+
+const MAX_TIME = 30;
+const Container = styled.div`
+  position: relative;
+  overflow: hidden;
+  margin-bottom: 1rem;
+  padding: 0.625rem;
+  border: 2px solid hsla(0, 0%, 100%, 18.75%);
+  border-radius: 1.5rem;
+  text-align: center;
+  font-weight: ${({ theme }) => theme.fontWeights.bold};
+
+  &::after {
+    content: '';
+    position: absolute;
+    top: 0.125rem;
+    left: 0.125rem;
+    width: ${({ theme: { time } }) => `calc(${(time / MAX_TIME) * 100}% - 0.25rem)`};
+    height: calc(100% - 0.25rem);
+    background: linear-gradient(to right, #f27121, #e94057, #8a2387);
+    border-radius: 1.5rem;
+    z-index: -100;
+  }
+`;
 
 function Timer() {
   const dispatch = useDispatch();
@@ -20,7 +45,11 @@ function Timer() {
 
   return (
     <p>
-      {time}
+      <ThemeProvider theme={ { time } }>
+        <Container>
+          <span>{time}</span>
+        </Container>
+      </ThemeProvider>
     </p>
   );
 }
