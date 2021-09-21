d632477784dc973715286f2b232eebe31c2b9623
Adryan Rosa <adryanrosa374@gmail.com>
2021-09-21 15:19:31 -0300

    Ajusta Questions Next

--- src/components/styled/Next.js
+++ src/components/styled/Next.js
@@ -4,7 +4,7 @@ const Next = styled.button`
   display: block;
   background: linear-gradient(to right, hsl(198, 60%, 50%), hsl(176, 68%, 64%));
   margin: 2rem auto;
-  padding: 0.875rem 2.5rem;
+  padding: 0.75rem 2.5rem;
   border: none;
   border-radius: 2.5rem;
   color: ${({ theme }) => theme.colors.neutral100};
